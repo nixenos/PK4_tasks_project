8b081a411346169416ce15357bba2934ae194673
Wojciech Janota <janota241@gmail.com>
2021-04-10 22:29:07 +0200

    [PROST-4] Crude event class prototype

--- Projekt/calendar/includes/eventModel.h
+++ Projekt/calendar/includes/eventModel.h
@@ -1,9 +1,16 @@
 #pragma once
 #include <string>
+#include "date.h"
 
 namespace calendar{
+
+typedef enum repeatCycle {Daily, Weekly, Monthly, Annually} repeatCycle;
+
 	class event{
-		private:
-			int date;
+	private:
+		date evDate;
+		std::string evName;
+		repeatCycle evRepeat;
+
 	};
 }
